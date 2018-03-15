10fae219d6542addda8158542ee9e1e9487009d8
Diogo Ferreira Martins <diogofm99@gmail.com>
2018-03-15 17:54:45 +0000

    New Program - Dices

---
+++ TestRandom/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAula05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfDices;
+            Random randomNumber = new Random();
+            int sum = 0;
+
+            Console.WriteLine("Insert Number of Dices: ");
+            numberOfDices = Convert.ToInt32(Console.ReadLine());
+             for( int i = 0; i < numberOfDices; i++)
+            {
+                sum += randomNumber.Next(1,7);
+            }
+
+            Console.WriteLine("A soma dos dados é igual a: " + sum);
+        }
+    }
+}
